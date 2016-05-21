29e1defe5495297d58223df04a8abc7bf677ebd8
liuawei <liuweijun@guoknag.com>
2016-05-21 17:54:48 +0800

    1：更新Json相关内容

--- src/main/java/com/liuawei/json/GsonUtil.java
+++ src/main/java/com/liuawei/json/GsonUtil.java
@@ -1,5 +1,33 @@
 package com.liuawei.json;
 
-public class GsonUtil {
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.Map;
+
+
+import com.google.gson.reflect.TypeToken;
+import com.liuawei.json.modle.Student;
+
+public final class GsonUtil {
+
+	/**
+	 * 转化为list对象
+	 */
+	public final static Type CONVERT_JSON_LISTS_ENTITY = new TypeToken<Collection<Class<?>>>() {}.getType();
+
+	/**
+	 * 转化为array对象
+	 */
+	public final static Type CONVERT_JSON_ARRAYS_ENTITY = new TypeToken<Class<?> []>() {}.getType();
+	
+	/**
+	 * 转化为map对象
+	 */
+	public final static Type CONVERT_JSON_MAP_ENTITY = new TypeToken<Map<Class<?>, Class<?>>>() {}.getType();
+	
+	
+	Type getListEntity(Class<?> clazz){
+		return new TypeToken<Collection<Class<?>>>() {}.getType();
+	}
 
 }
