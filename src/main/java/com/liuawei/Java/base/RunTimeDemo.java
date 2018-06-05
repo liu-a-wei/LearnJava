44a8f084528f5256e28e37c73d193efbd4945007
liuawei <weijun.liu@lerke.cn>
2018-06-05 14:35:25 +0800

    1:

--- src/main/java/com/liuawei/Java/base/RunTimeDemo.java
+++ src/main/java/com/liuawei/Java/base/RunTimeDemo.java
@@ -1,5 +1,7 @@
 package com.liuawei.Java.base;
 
+import java.io.IOException;
+
 public class RunTimeDemo {
 
 	public static void main(String[] args) {
@@ -7,6 +9,16 @@ public class RunTimeDemo {
 		System.out.println("jvm总共内存" + run.totalMemory());
 		System.out.println("jvm最大内存" + run.maxMemory());
 		System.out.println("jvm空闲内存" + run.freeMemory());
-		System.out.println("jvm空闲内存" + run.availableProcessors());
+		System.out.println("可用进程" + run.availableProcessors());
+		System.out.println();
+		
+		try {
+			Process process = run.exec("cmd");
+			System.out.println("执行命令!"+process.toString());
+			System.out.println("isAlive!"+process.isAlive());;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
