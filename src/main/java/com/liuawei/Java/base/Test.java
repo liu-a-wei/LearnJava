763aa3142e2bfe3415191997e0e1738a0a5d1e59
liuawei <weijun.liu@lerke.cn>
2018-11-01 19:02:46 +0800

    1:update

--- src/main/java/com/liuawei/Java/base/Test.java
+++ src/main/java/com/liuawei/Java/base/Test.java
@@ -1,8 +1,37 @@
 package com.liuawei.Java.base;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Test {
 
 	public static void main(String[] args) {
+		
+		List<Map<String, Object>> mapList = new ArrayList<>();
+		Map<String, Object> map = null;
+		map = new HashMap<>();
+		map.put("id", 1);
+		map.put("key", "value");
+		mapList.add(map);
+		map = new HashMap<>();
+		map.put("id", 2);
+		map.put("key", "value2");
+		mapList.add(map);
+		int[] arr = mapList.stream().mapToInt(m->{
+			return (int) m.get("id");
+		}).toArray();
+
+		System.out.println(arr[0]+""+arr[1]);
+		
+		
+		
+		
+		
+		
+		
+		
 
 		String str1 = "普付宝微信";
 		String str2 = "威富通微信";
